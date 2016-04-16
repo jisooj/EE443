967b4b2043c3fc476b9b00b5a4982d3c9aecac51
JISOO JUNG <jisooj@uw.edu>
2016-04-16 16:13:34 -0700

    convolve.s version 3

--- hw/JakeGarrison_JisooJung_Lab_3/convolve.s
+++ hw/JakeGarrison_JisooJung_Lab_3/convolve.s
@@ -24,35 +24,32 @@
 
 convolve:
 	add r2, r0, r0		# sum = 0
-	stw r6, r8 			# counter = N, decr by 1
-
+	stw r6, r8 			# counter = N, will decr by 1
 	muli r6, r6, 4		# N *= 4
-	add  r5, r5, r6 	# h += N. h points one word past the last element 
+	add r5, r5, r6 		# h += N. h points one word past the last element 
 	subi r5, r5, 4		# h--. h now points to the last element
-
-	add r9, r4, r6		# r9 = x + N 
+	add r9, r4, r6		# r9 = x + N. Used for MOD purpose.
 						# Points to one word past the last element of x. 
-						# Used for MOD purpose
-
 	muli r7, r7, 4		# i *= 4
 	add  r4, r4, r7		# x += startIndex
 	
 	LOOP:
-		mul  r10, 0(r4), 0(r5)	# temp = h(n) * x(n - k)
-		add  r2, r2, r10		# sum += temp
-		
+		mul r10, 0(r4), 0(r5)	# temp = h(n) * x(n - k)
+		add r2, r2, r10			# sum += temp
 		addi r4, r4, 4			# x++
 		subi r5, r5, 4			# h--
 		subi r8, r8, 1			# counter--
-		bge  r4, r9, MOD		# if (x >= counter)
-		jmp CONDITION
+		bge r4, r9, MOD			# if (x >= r9) { reset x to &x[0] } 
+								# else { CONDITION }
+		jmp CONDITION			# jump to loop condition
 		
 	CONDITION:
-		bgt  r8, r0, LOOP		# if (counter > 0) { LOOP } else { DONE }
+		bgt r8, r0, LOOP		# if (counter > 0) { LOOP } else { DONE }
+		jmp END
 		
 	MOD:
-		sub r4, r4, r6
-		jmp CONDITION
+		sub r4, r4, r6			# x = &x[0]		
+		jmp CONDITION			# jump to loop condition
 		
 	END:
-		ret 
+		ret 					# return sum
