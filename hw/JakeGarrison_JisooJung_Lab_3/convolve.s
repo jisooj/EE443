9fa71da6f0bbf484e22c134c86fcc60bcf64043f
JISOO JUNG <jisooj@uw.edu>
2016-04-18 23:28:56 -0700

    code update

--- hw/JakeGarrison_JisooJung_Lab_3/convolve.s
+++ hw/JakeGarrison_JisooJung_Lab_3/convolve.s
@@ -15,41 +15,44 @@
 
 # Local variables:
 #	r2 = sum, return value
-#	r8 = counter, will decremented by 1. Used in looping condition.
-#	r9 = points one word past the last element of x, used for MOD purpose
-#	r10 = temp storage for multiplied elements
+#	r7 = After startIndex is used to move x pointer,
+#	it's set to 4*N to be used in MOD.
+#	r8 = one element past the x[N - 1]
+#	r9 = temp1 = *x
+#	r10 = temp2 = *h
+#	r11 = temp3 = temp1 * temp2
 
 # Return value:
 #	r2 = sum
 
 convolve:
-	add r2, r0, r0		# sum = 0
-	stw r6, r8          # counter = N, will decr by 1
+	add  r2, r0, r0		# sum = 0
 	muli r6, r6, 4		# N *= 4
-	add r5, r5, r6 		# h += N. h points one word past the last element 
-	subi r5, r5, 4		# h--. h now points to the last element
-	add r9, r4, r6		# r9 = x + N. Used for MOD purpose.
-                        # Points to one word past the last element of x. 
-	muli r7, r7, 4		# i *= 4
+	add  r5, r5, r6 	# h += N. r5 = one element past h[N - 1]
+	subi r5, r5, 4		# h--. r5 = h[N - 1]
+	add  r8, r6, r4		# r8 = one element past the x[N - 1]
+	muli r7, r7, 4		# startIndex *= 4
 	add  r4, r4, r7		# x += startIndex
-	
+	add  r7, r0, r6		# r7 = N. Used in MOD
+
 	LOOP:
-		mul r10, 0(r4), 0(r5)	# temp = h(n) * x(n - k)
-		add r2, r2, r10			# sum += temp
-		addi r4, r4, 4			# x++
-		subi r5, r5, 4			# h--
-		subi r8, r8, 1			# counter--
-		bge r4, r9, MOD			# if (x >= r9) { reset x to &x[0] } 
-								# else { CONDITION }
-		jmp CONDITION			# jump to loop condition
-		
-	CONDITION:
-		bgt r8, r0, LOOP		# if (counter > 0) { LOOP } else { DONE }
-		jmp END
-		
+		ldw  r9,  0(r4)		# temp1 = *x
+		ldw  r10, 0(r5)		# temp2 = *h
+		mul  r11, r9, r10	# temp3 = temp1 * temp2 = h(n) * x(n - k)
+		add  r2, r2, r11	# sum += temp
+		addi r4, r4, 4		# x++
+		subi r5, r5, 4		# h--
+		subi r6, r6, 4		# N -= 4
+		bge  r4, r8, MOD	# if (x >= r8) { MOD } else { CONDITION }
+		jmpi CONDITION		# jump to loop condition
+
 	MOD:
-		sub r4, r4, r6			# x = &x[0]		
-		jmp CONDITION			# jump to loop condition
-		
+		sub  r4, r4, r7		# x = first element address
+		jmpi CONDITION		# jump to loop condition
+
+	CONDITION:
+		bgt  r6, r0, LOOP	# if (N > 0) { LOOP } else { END }
+		jmpi END			# jump to return statement
+
 	END:
-		ret 					# return sum
+		ret					# return sum
